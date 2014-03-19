fca4b4e08abac4fba304ccb918b5cbd825d76c04
Edward Soo <paktiksoo@gmail.com>
2014-03-18 19:19:42 -0700

    graph vertex draggable

--- web/js/graph.js
+++ web/js/graph.js
@@ -30,8 +30,12 @@
         // if the canvas is ever resized, screenSize should be called again with
         // the new dimensions
         particleSystem.screenSize(canvas.width, canvas.height);
+
         // leave an extra 80px of whitespace per side
         particleSystem.screenPadding(20); 
+
+        // set up some event handlers to allow for node-dragging
+        that.initMouseHandling()
       },
       
       // 
@@ -120,7 +124,58 @@
             ctx.fillText(node.name, pt.x, pt.y + 4*r);
           }
         })    			
-      }
+      },
+
+      initMouseHandling:function(){
+        // no-nonsense drag and drop (thanks springy.js)
+        var dragged = null;
+
+        // set up a handler object that will initially listen for mousedowns then
+        // for moves and mouseups while dragging
+        var handler = {
+          clicked:function(e){
+            var pos = $(canvas).offset();
+            _mouseP = arbor.Point(e.pageX-pos.left, e.pageY-pos.top)
+            dragged = particleSystem.nearest(_mouseP);
+
+            if (dragged && dragged.node !== null){
+              // while we're dragging, don't let physics move the node
+              dragged.node.fixed = true
+            }
+
+            $(canvas).bind('mousemove', handler.dragged)
+            $(window).bind('mouseup', handler.dropped)
+
+            return false
+          },
+          dragged:function(e){
+            var pos = $(canvas).offset();
+            var s = arbor.Point(e.pageX-pos.left, e.pageY-pos.top)
+
+            if (dragged && dragged.node !== null){
+              var p = particleSystem.fromScreen(s)
+              dragged.node.p = p
+            }
+
+            return false
+          },
+
+          dropped:function(e){
+            if (dragged===null || dragged.node===undefined) return
+            if (dragged.node !== null) dragged.node.fixed = false
+            dragged.node.tempMass = 1000
+            dragged = null
+            $(canvas).unbind('mousemove', handler.dragged)
+            $(window).unbind('mouseup', handler.dropped)
+            _mouseP = null
+            return false
+          }
+        }
+        
+        // start listening
+        $(canvas).mousedown(handler.clicked);
+
+      },
     }
     return that;
   }    
