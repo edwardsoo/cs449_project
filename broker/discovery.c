c6a2bda2c74dbc3863c9c9689b6c03d65cc349ac
Edward Soo <paktiksoo@gmail.com>
2014-03-31 03:27:16 -0700

    refactor discovery program

--- broker/discovery.c
+++ broker/discovery.c
@@ -5,27 +5,33 @@
 
 #define STR_LEN 0x101
 
-void get_response (void*, zhash_t*, zhash_t*, zframe_t*, char*);
+void send_get_response (void*, zhash_t*, zframe_t*, char*);
 
-typedef struct zhash_foreach_arg {
-  char addr[STR_LEN];
-  int num_conn;
-} zhash_foreach_arg_t;
+// Backend address, peer-publish address and list of backends connected
+typedef struct broker {
+  char *be_addr;
+  char *pub_addr;
+  zlist_t *backends;
+} broker_t;
 
-void free_broker_table_entry (void *data) {
-  zlist_t *backends = (zlist_t*) data;
-  while (zlist_size (backends)) {
-    zframe_destroy (zlist_pop (backends));
+void broker_free (void *data) {
+  broker_t *broker = (broker_t*) data;
+
+  while (zlist_size (broker->backends)) {
+    zframe_destroy (zlist_pop (broker->backends));
   }
+  free (broker->be_addr);
+  free (broker->pub_addr);
+  free (broker);
 }
 
 int find_least_used_broker (const char *key, void *item, void *argument) {
-  zhash_foreach_arg_t *arg = (zhash_foreach_arg_t*) argument;
-  zlist_t *backends = (zlist_t*) item;
+  broker_t* broker = (broker_t*) item;
+  broker_t** lu_broker = (broker_t**) argument;
 
-  if (!strlen (arg->addr) || arg->num_conn > zlist_size (backends)) {
-    strcpy (arg->addr, key);
-    arg->num_conn = zlist_size (backends);
+  if ((*lu_broker) == NULL ||
+      zlist_size((*lu_broker)->backends) > zlist_size (broker->backends)) {
+    *lu_broker = broker;
   }
   return 0;
 }
@@ -34,8 +40,8 @@ int main (int argc, char* argv[]) {
   zctx_t *ctx;
   zmsg_t *msg, *be_msg;
   zframe_t *identity, *frame, *op, *pend_back;
-  zlist_t *backends;
-  char str[STR_LEN], *idstr, *addr;
+  broker_t *broker, *lu_broker;
+  char str[STR_LEN], *idstr, *be_addr;
   int rv;
 
   if (argc != 2) {
@@ -51,7 +57,7 @@ int main (int argc, char* argv[]) {
   printf ("\nDiscovery: binding to %s\n", str);
   zsocket_bind (sock, str);
 
-  // Maps broker address to list of backends connected to broker
+  // Maps broker address to a broker_t struct
   zhash_t *broker_table = zhash_new ();
   // Maps backend ID (hex string) to broker address assigned
   zhash_t *backend_table = zhash_new ();
@@ -71,77 +77,84 @@ int main (int argc, char* argv[]) {
     op = zmsg_pop(msg);
 
     if (zframe_streq (op , DISC_ADD)) {
-      // Msg: ["ADD"] -> [Broker Address] -> NULL
+      // Msg: ["ADD"] -> [Broker BE Address] -> [Broker Peer-Pub Address] -> NULL
+      broker = malloc (sizeof (broker_t));
+      broker->backends = zlist_new ();
       frame = zmsg_pop (msg);
-      addr = zframe_strdup (frame);
-      rv = zhash_insert (broker_table, addr, zlist_new ());
-      if (rv == -1) {
-        printf ("\nDiscovery: broker address %s already in table\n", addr);
-      }
-      zhash_freefn (broker_table, addr, free_broker_table_entry);
+      broker->be_addr = zframe_strdup (frame);
+      // zframe_destroy (&frame);
+      // frame = zmsg_pop (msg);
+      // broker->pub_addr = zframe_strdup (frame);
 
-      // If there are backend waiting for a broker, reply now
-      if (zlist_size (pend_backends)) {
-        printf ("\nDiscovery: replies to pending backend\n");
-        while (zlist_size (pend_backends)) {
-          pend_back = zlist_pop (pend_backends);
-          zframe_print(pend_back, "pending backend:");
-          get_response (sock, broker_table, backend_table,
-              pend_back, addr);
-          zframe_destroy (&pend_back);
+      rv = zhash_insert (broker_table, broker->be_addr, broker);
+      if (rv == -1) {
+        printf ("\nDiscovery: broker address %s already in table\n", broker->be_addr);
+        broker_free (broker);
+
+      } else {
+        zhash_freefn (broker_table, broker->be_addr, broker_free);
+
+        // If there are backend waiting for a broker, reply now
+        if (zlist_size (pend_backends)) {
+          printf ("\nDiscovery: replies to pending backend\n");
+          while (zlist_size (pend_backends)) {
+            pend_back = zlist_pop (pend_backends);
+            zframe_print(pend_back, "pending backend:");
+            send_get_response (sock, backend_table, pend_back, broker->be_addr);
+            zlist_append (broker->backends, pend_back);
+          }
         }
       }
 
-      free (addr);
       zframe_destroy (&frame);
 
     } else if (zframe_streq (op, DISC_DELETE)) {
       // Msg: ["DELETE"] -> [Broker Address] -> NULL
       frame = zmsg_pop (msg);
-      addr = zframe_strdup (frame);
-      backends  = (zlist_t*) zhash_lookup (broker_table, addr);
-      if (backends) {
-        while (zlist_size (backends)) {
+      be_addr = zframe_strdup (frame);
+      broker = (broker_t*) zhash_lookup (broker_table, be_addr);
+      if (broker) {
+        while (zlist_size (broker->backends)) {
           
           be_msg = zmsg_new ();
           zmsg_pushstr (be_msg, DISC_STOP);
-          zmsg_wrap (be_msg, zlist_pop (backends));
+          zmsg_wrap (be_msg, zlist_pop (broker->backends));
           printf ("\nDiscovery: notifying backend of broker termination\n");
           zmsg_dump (be_msg);
           zmsg_send (&be_msg, sock);
           zhash_delete (backend_table, idstr);
 
         }
-        zhash_delete (broker_table, addr);
+        zhash_delete (broker_table, be_addr);
 
       } else {
-        printf ("\nDiscovery: cannot find broker address %s in table\n", addr);
+        printf ("\nDiscovery: cannot find broker address %s in table\n", be_addr);
       }
-      free (addr);
+      free (be_addr);
       zframe_destroy (&frame);
 
     } else if (zframe_streq (op, DISC_PUT)) {
       // Msg: ["PUT"] -> NULL
-      addr = zhash_lookup (backend_table, idstr);
-      if (addr) {
+      be_addr = zhash_lookup (backend_table, idstr);
+      if (be_addr) {
         printf ("\nDiscovery: found broker address %s for backend ID %s\n",
-          addr, idstr);
+          be_addr, idstr);
 
-        backends = (zlist_t*) zhash_lookup (broker_table, addr);
+        broker = (broker_t*) zhash_lookup (broker_table, be_addr);
 
-        if (!backends) {
-          printf ("\nDiscovery: cannot map broker address %s to list\n", addr);
+        if (!broker) {
+          printf ("\nDiscovery: cannot find broker address %s in table\n", be_addr);
 
         } else {
           // Find the frame with same ID on list and remove it
-          frame = (zframe_t*) zlist_first (backends);
+          frame = (zframe_t*) zlist_first (broker->backends);
           while (frame) {
             if (zframe_eq (frame, identity)) {
-              zlist_remove (backends, frame);
+              zlist_remove (broker->backends, frame);
               zframe_destroy (&frame);
               break;
             }
-            frame = zlist_next (backends);
+            frame = zlist_next (broker->backends);
           }
         }
         zhash_delete (backend_table, idstr);
@@ -162,12 +175,13 @@ int main (int argc, char* argv[]) {
 
       if (zhash_size (broker_table)) {
         // Find the broker with least number of backend connection
-        zhash_foreach_arg_t arg = {"", 0};
-        zhash_foreach (broker_table, find_least_used_broker, &arg);
-        printf ("Least used broker: address %s, %d connections\n",
-            arg.addr, arg.num_conn);
+        lu_broker = NULL;
+        zhash_foreach (broker_table, find_least_used_broker, &lu_broker);
+        printf ("Least used broker: address %s, %ld connections\n",
+            lu_broker->be_addr, zlist_size (lu_broker->backends));
 
-        get_response (sock, broker_table, backend_table, identity, arg.addr);
+        send_get_response (sock, backend_table, identity, lu_broker->be_addr);
+        zlist_append (lu_broker->backends, zframe_dup (identity));
 
       } else {
         // Put backend on waiting list
@@ -188,9 +202,8 @@ int main (int argc, char* argv[]) {
   return 0;
 }
 
-void get_response (
-    void* sock, zhash_t* brokers, zhash_t *backends,
-    zframe_t* identity, char *addrstr
+void send_get_response (
+    void* sock, zhash_t *backends, zframe_t* identity, char *addrstr
     ) {
 
   zframe_t *frame;
@@ -200,18 +213,13 @@ void get_response (
 
   strcpy (addr, addrstr);
 
-  // Update broker table
-  zlist_t *backend_list = zhash_lookup (brokers, addr);
-  frame = zframe_dup (identity);
-  zlist_append (backend_list, frame);
-
   // Update backend table
   printf ("\nDiscovery: inserts [%s]->[%s] into backend table\n", idstr, addr);
   rv = zhash_insert (backends, idstr, addr);
   if (rv == -1) {
     printf ("\nDiscovery: backend ID %s already in table\n", idstr);
   }
-  zhash_freefn (backends, idstr, NULL);
+  zhash_freefn (backends, idstr, free);
   free (idstr);
 
   zmsg_t *be_msg = zmsg_new ();
